2e1dc7a39ae7fc0953c798d3ab3f1b315d868e88
anaumchik <alesandro.ocx@mail.ru>
2019-04-17 10:58:46 +0300

    Домашняя работа 3

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int s = 3700 * 24 * 7 * 8; //добавил врем для лучшего вывода информации
+        int sec;
+        int m;
+        int min, h;
+        int hour;
+        int day, d;
+        int week, w;
+        int mo;
+
+        sec = s % 60;
+        m = (s - sec) / 60;
+        min = m % 60;
+        h = (m - min) / 60;
+        hour = h % 24;
+        d = (h - hour) / 24;
+        day = d % 7;
+        w = (d - day) / 7;
+        week = w % 4;
+        mo = (w - week) / 4;
+
+        System.out.println(mo + " месяцев " + w + " недель " + d + " дней " + h + " часов " + min + " минут " + sec + " секунд ");
+    }
+
+
 }
