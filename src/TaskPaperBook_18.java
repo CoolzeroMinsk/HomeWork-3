0826073157dc119c562d65a63b783504447fc777
Yoriy Yakovlev <coolzero@bk.ru>
2019-04-29 13:00:39 +0300

    Задание 18 из тетради

--- src/TaskPaperBook_18.java
+++ src/TaskPaperBook_18.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TaskPaperBook_17 {
+public class TaskPaperBook_18 {
     public static void main(String[] args) {
         //вводим текст для поиска знаков препинания
         String taskString = "Скоро весь сад, согретый солнцем, обласканный, ожил, и капли росы, как алмазы, засверкали на листьях; и старый, давно запущенный сад в это утро казался таким молодым, нарядным ";
