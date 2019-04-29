3f5b5eb53838c4d1767e279abd98556eab12b6cd
Yoriy Yakovlev <coolzero@bk.ru>
2019-04-29 17:27:19 +0300

    завыфвфв

--- src/TaskPaperBook_18.java
+++ src/TaskPaperBook_18.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+        import java.util.regex.Matcher;
+        import java.util.regex.Pattern;
 
 public class TaskPaperBook_18 {
     public static void main(String[] args) {
         //вводим текст для поиска знаков препинания
         String taskString = "Скоро весь сад, согретый солнцем, обласканный, ожил, и капли росы, как алмазы, засверкали на листьях; и старый, давно запущенный сад в это утро казался таким молодым, нарядным ";
-        Pattern pattern = Pattern.compile("[!.,;:?\\-]"); //создаем шаблон поиска
-        Matcher matcher = pattern.matcher(taskString);
-        int count = 0;
-        while (matcher.find()) {
-            count++;
+        Pattern pattern = Pattern.compile("[!.,;:?\\-]"); //создаем шаблон поиска символов
+        Matcher matcher = pattern.matcher(taskString);    //передаем текст для поиска соответствий шаблону
+        int count = 0;                                    //
+        while (matcher.find()) {                          //добавляем цикл поиска знаков препинания
+            count++;                                      //
         }
-        System.out.println(count);
+        System.out.println(count);                        //выводим на экран получившееся количество знаков
     }
 }
