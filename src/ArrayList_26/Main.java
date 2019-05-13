4d719523ed174b33e6670969bf340ba82e5ba526
Coolzero <coolzero@bk.ru>
2019-05-13 12:32:48 +0300

    Задание 26

---
+++ src/ArrayList_26/Main.java
@@ -0,0 +1,30 @@
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>(10);//Создаем коллекцию с 10-ью элементами
+
+        for (int i = 0; i < 10; i++) {                          //заполняем случайными числами
+            int x = ((int) (Math.round(Math.random() * 10)));
+            arrayList.add(i, x);
+
+        }
+        System.out.println(Arrays.asList("Наша коллекция имеет вид:" + arrayList));    //выводим получившийся результат
+
+        for (Iterator<Integer> iterator = arrayList.iterator(); iterator.hasNext(); ) {//Пробегаем итератором
+            Integer integer = iterator.next();                                             //по нашей коллекции
+            if (integer <= 3) {                                                        //и убираем из неЁ элементы
+                iterator.remove();                                                     //меньше "тройки"
+            }
+        }
+        System.out.println("Список без плохих оценок выглядит следующим образом:" + Arrays.asList(arrayList));                                //Выводим получившийся результат
+    }
+}
+
+
+
